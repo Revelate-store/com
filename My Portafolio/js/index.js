45049196e14a482192faa282195bd170e825bf2e
Revelate-store <thenintraxx189@gmail.com>
2026-04-14 21:53:12 -0400

    Add files via upload

---
+++ My Portafolio/js/index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  document.getElementById("menu").style.opacity = "1";
+  document.getElementById("menu").style.transition = "1s";
+
+}
+function myFunction1() {
+  document.getElementById("menu").style.opacity = "0";
+
+}
+
